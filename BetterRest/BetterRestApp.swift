a301dea9853501c5e7059a5407ba1f8e58b94ecf
Fernando Gomez <redchameleonapps@gmail.com>
2023-01-12 16:37:38 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Fernando Gomez on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
